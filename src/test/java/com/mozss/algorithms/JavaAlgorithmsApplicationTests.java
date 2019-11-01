b019283f4f69daca094503138c0465cb3895d0d3
mozss <mozss@outlook.com>
2019-11-01 16:30:48 +0800

    Initial commit

---
+++ src/test/java/com/mozss/algorithms/JavaAlgorithmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mozss.algorithms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAlgorithmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
