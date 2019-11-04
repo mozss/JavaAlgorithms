37d7647fa02ff17befab26a1cf8b0d05683f9ddf
mozss <mozss@outlook.com>
2019-11-04 17:17:01 +0800

    usual

--- src/main/java/com/mozss/algorithms/sort/tjj/BucketSort.java
+++ src/main/java/com/mozss/algorithms/sort/tjj/BucketSort.java
@@ -3,24 +3,19 @@ package com.mozss.algorithms.sort.tjj;
 public class BucketSort {
     /*
      * 桶排序
-     *
      * 参数说明：
      *     a -- 待排序数组
      *     max -- 数组a中最大值的范围
      */
     public static void bucketSort(int[] a, int max) {
         int[] buckets;
-
         if (a==null || max<1)
             return ;
-
         // 创建一个容量为max的数组buckets，并且将buckets中的所有数据都初始化为0。
         buckets = new int[max];
-
         // 1. 计数
         for(int i = 0; i < a.length; i++)
             buckets[a[i]]++;
-
         // 2. 排序
         for (int i = 0, j = 0; i < max; i++) {
             while( (buckets[i]--) >0 ) {
@@ -33,15 +28,15 @@ public class BucketSort {
     public static void main(String[] args) {
         int i;
         int a[] = {8,2,3,4,3,6,6,3,9};
-
-        System.out.printf("before sort:");
+        System.out.println("======排序之前======");
         for (i=0; i<a.length; i++)
             System.out.printf("%d ", a[i]);
         System.out.printf("\n");
 
-        bucketSort(a, 10); // 桶排序
+        //排序算法处理
+        bucketSort(a, 10);
 
-        System.out.printf("after  sort:");
+        System.out.println("======排序之后======");
         for (i=0; i<a.length; i++)
             System.out.printf("%d ", a[i]);
         System.out.printf("\n");
