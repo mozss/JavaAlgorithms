c5c62752f6363f698ddb7acfe09f8afb8c1c3f08
mozss <mozss@outlook.com>
2019-11-09 14:00:35 +0800

    usual

--- src/main/java/com/mozss/algorithms/data_structures/tree/BinTree.java
+++
@@ -1,4 +0,0 @@
-package com.mozss.algorithms.data_structures.tree;
-
-public class BinTree {
-}
---
+++ src/main/java/com/mozss/algorithms/data_structures/tree/redblack/RedBlackTree.java
@@ -0,0 +1,4 @@
+package com.mozss.algorithms.data_structures.tree.redblack;
+
+public class RedBlackTree {
+}
