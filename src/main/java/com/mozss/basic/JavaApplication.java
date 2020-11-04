5d57fb3c9e0c8f1b0d2d0fad08b81f1fa7fe1a8c
mozss <mozss1024@gmail.com>
2020-11-04 09:50:01 +0800

    修改项目名称

--- src/main/java/com/mozss/basic/JavaApplication.java
+++ src/main/java/com/mozss/basic/JavaApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JavaAlgorithmsApplication {
+public class JavaApplication {
     public static void main(String[] args) {
-        SpringApplication.run(JavaAlgorithmsApplication.class, args);
+        SpringApplication.run(JavaApplication.class, args);
     }
 }
